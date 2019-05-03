6a722af6125ed8950e5f6c66806951d8b7b5d843
bahelhadj <elhadjbah@gmail.com>
2019-05-02 20:20:00 -0400

    look at that

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -26,30 +26,43 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  cb(arr.length);  // getLength passes the length of the array into the callback.
 }
+getLength(items, function(length) {
+  console.log(length)
+});
 
-function last(arr, cb) {
-  // last passes the last item of the array into the callback.
-}
+// function last(arr, cb) {
+//   cb(arr.length - 1);// last passes the last item of the array into the callback.
+// last(items, function(lastitem) {
+//   console.log(lastitem)
+// });
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y); // sumNums adds two numbers (x, y) and passes the result to the callback.
 }
+sumNums(2, 3, function(done) {
+  console.log(done);
+});
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 }
+  multiplyNums(4, 6, function(here) {
+    console.log(here);
+  });
+  // multiplyNums multiplies two numbers and passes the result to the callback
 
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
-}
-
-/* STRETCH PROBLEM */
-
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
-}
+  const isInArray = () => {
+    for (let i = 0; i < items.length; i++) {
+      if (item[i] === list) {
+        return true;
+      }
+    } return false;
+  };
+  cb(isInArray());
+};
+contains(items, 'NightKing', result => {
+  console.log(result);
+});
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,5 +1,10 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+let bah = "Javascript is hard bros and girls"
+function speak() {
+  return bah;
+}
+console.log(speak());
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
@@ -7,15 +12,32 @@
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+  let count = 0; 
+  return () => {
+    return ++count;
+  }
 };
+newcounter = counter();
+console.log(newcounter());
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+const counterFactory = (num1) => { 
+  const simple = {
+    increment: function () {
+      return num1 + 1;
+    } ,
+    decrement: function () {
+      return num1 - 1;
+    }
+  }
+  return simple;
+};
+console.log(counterFactory(1).increment());
+console.log(counterFactory(1).decrement());
+
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
